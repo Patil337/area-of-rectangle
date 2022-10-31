1c4bbc4728c8d5caa275c2c77f3bde87720efeb4
Rahul Uday Patil <88849833+Patil337@users.noreply.github.com>
2022-10-31 11:53:14 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,17 @@
+Q3 - Write a Java program to take the length and breadth of a rectangle and print its area.
+Sample Input : 7, 4
+Sample Output : 28
+
+code:
+
+public class Main {
+  public static void main(String[] args) {
+int length=7;
+int breadth=4;
+System.out.println("the length is:" +length);
+System.out.println("the breadth is:" +breadth);
+int area=length * breadth;
+
+    System.out.println("the ascii value of area is:"+area);
+  }
+}
